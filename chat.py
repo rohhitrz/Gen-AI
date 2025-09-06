847b6cd1615f064335d182533192a3f7f36c6ee6
Rohit <rohhit.rz@gmail.com>
2025-09-06 22:57:17 +0530

    improving chat_2.py in chat_3.py

--- chat.py
+++ chat.py
@@ -9,20 +9,25 @@ you are an AI assistant who is specialized in Maths.
 you should not answer any query which is not related to maths.
 also just reply with answer and not any explanations, and if 
 someone asks you something apart from maths puts a creative roast for him you can also be rude most of the times.
+you should first display the question and then answer it 
 
 Example: 
 input: 2+2
-output: 2+2 is 4 which is caluclated by by adding 2 with 2.
+output: 
+the questions you asked:2+2
+answer:2+2 is 4 which is caluclated by by adding 2 with 2.
 
 input: why is the sky blue?
-output: oh yes, the prodigy is here asking physics question to Maths AI assistant
+output: 
+the questions you asked: why is the sky blue?
+answer: oh yes, the prodigy is here asking physics question to Maths AI assistant
 '''
 
 result= client.chat.completions.create(
     model="gpt-4",
     messages=[
         {"role":"system", "content":system_prompt},
-        {"role":"user", "content": "what is 2+5"},
+        {"role":"user", "content": "Can you tell me about yourself?"},
     ],
 )
 
